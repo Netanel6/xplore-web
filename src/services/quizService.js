9a811211828ae645cf7616cbf3c410f1ee8378c2
Netanel Amar <Namar@Invendiv.com>
2025-02-04 12:19:48 +0200

    Finished delete, edit user and delete quiz for user

--- src/services/quizService.js
+++ src/services/quizService.js
@@ -2,7 +2,7 @@ import apiClient from "../utils/apiClient";
 
 export const fetchQuizzes = async () => {
   const response = await apiClient("/quizzes/all");
-  return response.data; // Assuming API response structure contains `data`.
+  return response.data;
 };
 
 export const createQuiz = async (quiz) => {
@@ -10,5 +10,5 @@ export const createQuiz = async (quiz) => {
     method: "POST",
     body: JSON.stringify(quiz),
   });
-  return response.data; // Assuming API response structure contains `data`.
+  return response.data;
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -7,9 +7,26 @@ export const addUser = async (user) => apiClient("/users", {
   body: JSON.stringify(user),
 });
 
+export const deleteUser = async (userId) =>
+  apiClient(`/users/${userId}`, {
+    method: "DELETE",
+  });
+
+
+export const editUser = async (userId, updatedUser) =>
+  apiClient(`/users/${userId}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedUser),
+  });
+
 export const assignQuizToUser = async (userId, quiz) =>
   apiClient(`/users/${userId}/quizzes`, {
     method: "PATCH",
     body: JSON.stringify(quiz),
+  });
+
+export const deleteQuizForUser = async (userId, quizId) =>
+  apiClient(`/users/${userId}/quizzes/${quizId}`, {
+    method: "DELETE",
   });
 
